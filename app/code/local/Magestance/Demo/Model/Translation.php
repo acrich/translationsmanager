ab2ee49da5c924b011f198a6edb17386cac5be51
shay <acrich@gmail.com>
2012-06-24 12:07:13 +0300

    accounted for missing store_ids and locales when saving translations

--- app/code/local/Magestance/Demo/Model/Translation.php
+++ app/code/local/Magestance/Demo/Model/Translation.php
@@ -11,14 +11,49 @@ class Magestance_Demo_Model_Translation extends Mage_Core_Model_Abstract
 	
 	public function createItem($item)
 	{
-		if (!array_key_exists('store_id', $item)) {
-			$item['store_id'] = 0;
+		if (array_key_exists('string_id', $item)) {
+			if (!array_key_exists('store_id', $item)) {
+				if (array_key_exists('locale', $item)) {
+					$stores = Mage::app()->getStores();
+					$found = false;
+					foreach ($stores as $store) {
+						if ($store->getConfig('general/locale/code') == $item['locale']) {
+							$found = true;
+							$item['store_id'] = $store->getId();
+							$this->_createItem($item);
+						}
+					}
+					if (!$found) {
+						$this->_createDefaultItem($item);
+					}
+				} else {
+					$this->_createDefaultItem($item);
+				}
+			} else {
+				if (!array_key_exists('locale', $item)) {
+					$item['locale'] = Mage::app()->getStore($item['store_id'])->getConfig('general/locale/code');
+				}
+				$this->_createItem($item);
+			}
+		} else {
+			return false;
 		}
+	}
+	
+	protected function _createDefaultItem($item)
+	{
+		$item['store_id'] = 0;
+		$item['locale'] = Mage::app()->getStore($item['store_id'])->getConfig('general/locale/code');
+		$this->_createItem($item);
+	}
+	
+	protected function _createItem($item)
+	{
 		if ($translation_id = $this->getIdByParams($item['string_id'], $item['store_id'])) {
 			$this->load($translation_id);
 			if (array_key_exists('translation', $item) && $item['translation'] != '') {
 				$this->setTranslation(serialize($item['translation']))
-					->save();
+				->save();
 				return $translation_id;
 			} else {
 				$this->delete();
@@ -29,9 +64,7 @@ class Magestance_Demo_Model_Translation extends Mage_Core_Model_Abstract
 				$this->setTranslation(serialize($item['translation']));
 				$this->setStringId($item['string_id']);
 				$this->setStoreId($item['store_id']);
-				if (array_key_exists('locale', $item)) {
-					$this->setLocale($item['locale']);
-				}
+				$this->setLocale($item['locale']);
 				$this->save();
 				return $this->getTranslationId();
 			} else {
