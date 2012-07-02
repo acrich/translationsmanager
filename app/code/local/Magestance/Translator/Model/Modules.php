07f9746978a3c4bf2a1a216c6cc15e870b139f65
shay <acrich@gmail.com>
2012-07-02 21:01:46 +0300

    added none option in modules list

--- app/code/local/Magestance/Translator/Model/Modules.php
+++ app/code/local/Magestance/Translator/Model/Modules.php
@@ -7,7 +7,7 @@ class Magestance_Translator_Model_Modules extends Varien_Object
 	{
 		$modules = Mage::getStoreConfig('advanced/modules_disable_output');
 		
-		$result = array();
+		$result = array(0 => array('value' => '', 'label' => 'None'));
 		foreach ($modules as $name => $disabled) {
 			if (!$disabled) {
 				$result[] = array('value' => $name, 'label' => $name);
